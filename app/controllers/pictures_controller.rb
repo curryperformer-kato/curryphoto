b063b740fcc350ad402ab9213121b371eff8350d
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-06-26 01:41:31 +0000

    first commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,53 @@
+class PicturesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_picture, only: [:edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    if params[:back]
+      @picture = Picture.new(pictures_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def create
+    @picture = Picture.new(pictures_params)
+    @blog.user_id = current_user.id
+    if @picture.save
+      redirect_to pictures_path, notice: "写真を投稿しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @picture.update(pictures_params)
+    redirect_to pictures_path, notice: "写真を更新しました！"
+  end
+
+  def confirm
+    @picture = Picture.new(pictures_params)
+    render :new if @picture.invalid?
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice: "写真を削除しました！"
+  end
+
+  private
+    def pictures_params
+      params.require(:picture).permit(:title, :content)
+    end
+    
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+end
